972c53c76833f4bc689ec61bfbfef09111c56007
Gerardagh <127221531+Gerardagh@users.noreply.github.com>
2023-09-29 14:45:21 +0300

    Update simpler_zibil.cpp

--- simpler_zibil.cpp
+++ simpler_zibil.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
 int main() {
-    // Declare
+    // Declaration 
     float num1, num2, result_add, result_sub, result_mul, result_div;
-    num1 = 25.5f; // Preset first number as a float
-    num2 = 65.7f;  // Preset second number as a float
-
+    //Preset Numbers
+    num1 = 25.5f; // Preset first number 
+    num2 = 65.7f;  // Preset second number 
+    // Operations here
     result_add = num1 + num2;
     result_sub = num1 - num2;
     result_mul = num1 * num2;
-
+    //Condition to not allowed division by Zero
     if (num2 != 0.0f) {
         result_div = num1 / num2;
     } else {
         std::cerr << "Error: Division by zero" << std::endl;
         return 1; 
     }
-
+    //Display Operations
     std::cout << "Addition: " << result_add << std::endl;
     std::cout << "Subtraction: " << result_sub << std::endl;
     std::cout << "Multiplication: " << result_mul << std::endl;
