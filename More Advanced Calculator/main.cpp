545434b76c4f2fe9b1f3394a7a35451bc30a9a3c
Engineer <davidmamik10@gmail.com>
2023-10-07 11:30:14 +0400

    07/10/2023, Homework #1 completed.
    Created a check system for substraction, to replace X with Y incase if X is greaten than Y which might result in negative sum.

--- More Advanced Calculator/main.cpp
+++ More Advanced Calculator/main.cpp
@@ -8,35 +8,51 @@ int main() {
     float sum;
     char sym;
 
+    // Отобразить сообщение с просьбой ввести оператор или 0 для выхода.
     cout<<"Insert |+| |-| |*| |/| 0 exit " <<endl;
     cin>> sym;
 
-    switch(sym){
+    // Оператор Switch для работы с разными операторами
+    switch(sym) {
         case '+':
-            cout<<"|+| Insert Number X and Y";
-            cin>>x;
-            cin>>y;
+            // Предложить пользователю ввести числа x и y для сложения.
+            cout << "|+| Insert Number X and Y";
+            cin >> x;
+            cin >> y;
+            // Выполните сложение и отобразит сумму
             sum = x + y;
-            cout<<"sum = "<<sum <<endl;
+            cout << "sum = " << sum << endl;
             break;
+
         case '-':
-            cout<<"|-| Insert Number X and Y"<<endl;
-            cin>>x;
-            cin>>y;
+            // Предложить пользователю ввести числа x и y для вычитания.
+            cout << "|-| Insert Number X and Y" << endl;
+            cin >> x;
+            cin >> y;
+            // Проверьте, больше ли x, чем y, и выполните вычитание соответственно.
+            double temp;
+            temp = x;
+            x = y;
+            y = temp;
             sum = x - y;
             cout<<"sum = "<<sum<<endl;
             break;
         case '*':
+            // Предложить пользователю ввести числа x и y для умножения.
             cout<<"|*| Insert Number X and Y"<<endl;
             cin>>x;
             cin>>y;
+            // Выполните умножение и отобразит сумму
             sum = x * y;
             cout<<"sum = "<<sum<<endl;
             break;
+
         case '/':
+            // Предложить пользователю ввести числа x и y для деления.
             cout<<"|/| Insert Number X and Y"<<endl;
             cin>>x;
             cin>>y;
+            // Выполните деление и отобразите частное.
             sum = x / y;
             cout<<"sum = "<<sum<<endl;
             break;
