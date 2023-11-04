8d8f8ce260a63250352fcba4942619c8af15ce8b
Engineer <davidmamik10@gmail.com>
2023-11-04 11:42:01 +0400

    Repository restructuring

---
+++ ASCII/CharacterToCode example/cmake-build-debug/CMakeFiles/Chraxyan.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Chraxyan.
+# This may be replaced when dependencies are built.
---
+++ ASCII/Simple ASCII convertor/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char code;
+    int x = 0;
+
+    cout << "Please Insert Character or Type '*' to break program" << endl;
+
+    while (true) {
+        cin >> code;
+        if (code == '*') {
+            break;
+        } else{
+            x = code;
+            cout<<"ASCII = "<<x<<endl;
+        }
+    }
+
+    return 0;
+}
