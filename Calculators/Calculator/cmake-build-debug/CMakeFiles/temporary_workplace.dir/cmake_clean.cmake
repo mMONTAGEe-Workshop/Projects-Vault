05d1ef6b33a02c26e98f21eda84ac31be14e63b2
Engineer <davidmamik10@gmail.com>
2023-11-04 11:47:12 +0400

    Repository restructuring

---
+++ Calculators/Calculator/cmake-build-debug/CMakeFiles/temporary_workplace.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temporary_workplace.dir/main.cpp.obj"
+  "CMakeFiles/temporary_workplace.dir/main.cpp.obj.d"
+  "libtemporary_workplace.dll.a"
+  "temporary_workplace.exe"
+  "temporary_workplace.exe.manifest"
+  "temporary_workplace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temporary_workplace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Calculators/Calculator/cmake-build-debug/CMakeFiles/temporary_workplace.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for temporary_workplace.
+# This may be replaced when dependencies are built.
